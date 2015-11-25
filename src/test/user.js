afed1af8876615fe32094fba51bb8d4f3e17bf7e
Pascal Weik <pascalweik@Pascals-MacBook-Pro-2.local>
2015-11-25 19:26:22 +0100

    Update user.js. Not working though

--- src/test/user.js
+++ src/test/user.js
@@ -1,14 +1,21 @@
-// var setup = require('./setup');
-// describe('Server', function () {
-//
-//   describe('', function(){
-//     beforeEach(setup.boot);
-//     afterEach(setup.shutdown);
-//     it('user iwas ',function(done){
-//           console.log("useranlegen");
-//
-//           done();
-//       })
-//     });
-//   });
-// });
+var setup = require('./setup');
+describe('User', function () {
+
+  describe('Signup', function(){
+    before(setup.boot);
+    after(setup.shutdown);
+    it('credentials are valid ',function(done){
+
+      var profile  ={ username: 'sweg', password: '123' };
+
+      setup.superagent
+          .post('http://localhost:'+setup.port+"/login")
+          .send(profile)
+          .set('Accept', 'application/json')
+          .end(function(err, res){
+            setup.expect(res.status).to.equal(200);
+            done();
+      })
+    });
+  });
+});
