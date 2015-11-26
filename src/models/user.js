4519d74bba82c569a3d370d60371b4c9818101c2
Immanuel Haag <Immi@Immanuels-MacBook-Pro.local>
2015-11-26 11:30:07 +0100

    new game.js, npm start throw errors! problem with ready() Function

--- src/models/user.js
+++ src/models/user.js
@@ -7,5 +7,8 @@ module.exports = mongoose.model('User',{
 	password: String,
 	email: String,
 	firstName: String,
-	lastName: String
+	lastName: String,
+	radiusForGames: Buffer,
+	perimeter: Number,
+	activated: Boolean
 });
