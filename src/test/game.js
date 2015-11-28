4df118220a9f318fba37e8754f5ce55934b28fe1
Immanuel Haag <Immi@Immanuels-MBP.fritz.box>
2015-11-28 14:06:56 +0100

    game test updated -> PUT don't work.

--- src/test/game.js
+++ src/test/game.js
@@ -4,8 +4,8 @@
 var expect = require('expect.js');
 var superagent= require('superagent');
 require = require('really-need');
-var username='Tester33';
-var email='tester33@test.de';
+var username='Tester4';
+var email='tester4@test.de';
 var password='secret';
 var host='http://localhost:3000/api/'
 var adress='Im Tesfall 23'
@@ -80,8 +80,8 @@ describe('Game-Testsuite', function () {
           .end(function(e, res){
               expect(e).to.eql(null)
               expect(res.status).to.eql(200);
-              expect(res.body.length).to.eql(71);
-              expect(res.body[69].userId).to.eql(userid);
+              expect(res.body.length).to.eql(2);
+              expect(res.body[1].userId).to.eql(userid);
               done()
           })
     });
@@ -99,6 +99,73 @@ describe('Game-Testsuite', function () {
           })
     });
   });
-  //TODO: Alle Einräge aus meiner DB löschen, --> zähler anpassen dann delete route, put route dann wieder delete
+  describe('update -second- game' , function(){
+ //TODO: Controller muss noch angepasst werden! 
+    it('with PUT by gameId: ',function(done){
+      superagent.put(host+'games/'+gameidnew)
+          .send('adress=NeueAdresse')
+          .end(function(e, res){
+            console.log(res.body);
+              expect(res.status).to.eql(200);
+              expect(res.body._id).to.eql(gameidnew);
+              expect(res.body.adress).to.eql('NeueAdresse')
+              done()
+          })
+    });
+  });
 
+
+  describe('delete games', function(){
+
+    it('with DELETE -first- game: ',function(done){
+      superagent.del(host+'games/'+gameidnew)
+          .type('form')  //Warum auch immer man form bei unserer api nehmen muss?
+          .auth(email, password) // x-www-form-urlencoded
+          .end(function(e, res){
+              expect(e).to.eql(null)
+              expect(res.status).to.eql(200);
+
+              done()
+          })
+    });
+    it('with GET first game should fail: ',function(done){
+      superagent.get(host+'games/'+gameidnew)
+          .end(function(e, res){
+              expect(e).to.eql(null)
+              expect(res.body).to.eql(null);
+              expect(res.status).to.eql(200);
+              done()
+          })
+    });
+    it('with DELETE -second- game: ',function(done){
+      superagent.del(host+'games/'+gameid)
+          .type('form')  //Warum auch immer man form bei unserer api nehmen muss?
+          .auth(email, password) // x-www-form-urlencoded
+          .end(function(e, res){
+
+              expect(e).to.eql(null)
+              expect(res.status).to.eql(200);
+
+              done()
+          })
+    });
+    it('DELETE user for second testrun : ',function(done){
+      superagent.del(host+'users/'+userid)
+          .type('form')  //Warum auch immer man form bei unserer api nehmen muss?
+          .auth(email, password) // x-www-form-urlencoded
+          .send('email='+email) // liegt das am Body parser?
+          .send('username='+username)
+          .send('password='+password)
+          .end(function(e, res){
+
+              expect(e).to.eql(null)
+              expect(res.status).to.eql(200);
+              expect(res.body.username).to.eql(username);
+              expect(res.body.email).to.eql(email);
+              var id = res.body._id;
+              expect(id.length).to.eql(24)
+              done()
+          })
+    });
+  });
 });
