6289d0b889c2243a2a7b93a8411cf256d7c7ac39
Immanuel Haag <immae1@users.noreply.github.com>
2016-01-13 23:08:33 +0100

    test4docker

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 var db = {
   production: 'mongodb://root:toor@192.168.99.100:27017/radcupProduction',
-  development: 'mongodb://root:toor@192.168.99.100:27017/radcupDevelopment',
+  development: 'mongodb://root:toor@db:27017/radcupDevelopment',
   test: 'mongodb://root:toor@192.168.99.100:27017/radcupTest'
 }
 
