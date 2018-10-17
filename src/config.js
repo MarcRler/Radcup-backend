3d101fe414070b600650248b75f52fdd7ecdf717
Haag Immanuel <ih038@hdm-stuttgart.de>
2018-10-17 21:57:01 +0200

    setted ip to docker machine ip of mongocon

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 /*Datenbank Konfiguartion, enthält ein dummy "dockerweb" welcher durch sed Skript
 bei dockerstart getauscht wird*/
 var db = {
-  production: 'mongodb://root:toor@172.17.0.4:27017/radcupProduction',
-  development: 'mongodb://root:toor@172.17.0.4:27017/radcupDevelopment',
-  test: 'mongodb://root:toor@172.17.0.4:27017/radcupTest'
+  production: 'mongodb://root:toor@172.17.0.2:27017/radcupProduction',
+  development: 'mongodb://root:toor@172.17.0.2:27017/radcupDevelopment',
+  test: 'mongodb://root:toor@172.17.0.2:27017/radcupTest'
 }
 
 module.exports = db[process.env.NODE_ENV || 'development']
